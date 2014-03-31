4cc63bd081aa5d81f1efd01be36e6e86f9b1077c
jaumejane90 <jaumejane90@gmail.com>
2014-03-31 12:32:29 +0200

    Rename AnalyzedFLSentence to FlAnalyzedSentence

---
+++ src/test/java/FlAnalyzedSentenceTest.java
@@ -0,0 +1,71 @@
+/**
+Copyright 2014 Jaume Jané 
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cat.tv3.eng.rec.fl.analyzed.sentence.test;
+
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+import cat.tv3.eng.rec.fl.analyzed.sentence.FlAnalyzedSentence;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+public class FlAnalyzedSentenceTest {
+	
+	private final static String FREELING_OUTPUT = "freeling_out.txt";
+	private static String outputFreeling;
+	private static FlAnalyzedSentence analizedFreelingSentence;
+	
+	@BeforeClass
+	public static void prepareFile(){
+		outputFreeling = "";
+		InputStream is = FlAnalyzedSentence.class.getClassLoader().getResourceAsStream(FREELING_OUTPUT);
+		outputFreeling = new Scanner(is,"UTF-8").useDelimiter("\\A").next();
+		
+		analizedFreelingSentence = new FlAnalyzedSentence(outputFreeling);		
+		
+	}
+	
+	@Test
+	public void testOriginalText() throws InterruptedException {				
+		assertEquals("A test method is a definitive procedure that produces a test result", analizedFreelingSentence.getOriginalText());	
+	}
+	
+	@Test
+	public void  testMorfologicText() throws InterruptedException {			
+		assertEquals("1 test method be 1 definitive procedure that produce 1 test result", analizedFreelingSentence.getMorfologicText());	
+	}
+	
+	@Test
+	public void  testPoSTaggedText() throws InterruptedException {			
+		assertEquals("Z NN NN VBZ Z JJ NN WDT VBZ Z NN NN", analizedFreelingSentence.getPoSTaggedText());	
+	}
+	
+	@Test
+	public void  testgetProbsMorfologicText() throws InterruptedException {			
+		assertEquals("0.99998 0.918129 1.0 1.0 0.99998 1.0 1.0 0.244662 1.0 0.99998 0.918129 0.843311", analizedFreelingSentence.getProbsMorfologicText());	
+	}
+	
+	
+
+
+	
+	
+}
